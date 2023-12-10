9c2aba240632eca1473b28953bda793066117450
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2023-12-10 18:08:39 +0200

    Time: 7 ms (69.20%), Space: 12.1 MB (22.26%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -16,8 +16,8 @@ public:
 
         while (curr) 
         {
-           while (curr->next && curr->val == curr->next->val) curr->next = curr->next->next;
-           curr = curr->next;
+           while (curr->next&&curr->val==curr->next->val) curr->next=curr->next->next;
+           curr=curr->next;
         }
 
         return head;
