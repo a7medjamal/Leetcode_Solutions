f954a026035072d66c773699754a265a7e12a04b
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-01-05 21:09:49 +0200

    Time: 6 ms (97.37%), Space: 15.7 MB (15.36%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        for(auto i:nums) mp[i]++;
+        vector<vector<int>>vp(nums.size()+1);
+        for(auto [a,b]:mp)
+        {
+           vp[b].push_back(a);
+        }
+        vector<int>ans;
+        for(int i = vp.size()-1;i>=0;i--)
+        {
+            for(auto j:vp[i])
+            {
+                if(k>0)
+                {
+                    ans.push_back(j);
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
