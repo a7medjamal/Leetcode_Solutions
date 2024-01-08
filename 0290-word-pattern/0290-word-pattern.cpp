3d0e26b12846b5d0262dabec42c903bc234359b2
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-01-08 20:41:37 +0200

    Time: 3 ms (43.75%), Space: 6.7 MB (72.00%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -4,17 +4,11 @@ public:
         unordered_map<char,string>mp;
         set<string>st;
         vector<string>v;
-        int i = 0,n = pattern.size(),m = s.size(); string temp;
-        for(i=0;i<m;i++)
-        {
-            if(s[i]==' ')
-            {
-                v.push_back(temp);
-                temp="";
-            }
-            else temp+=s[i];
-        }
-        v.push_back(temp); int o = v.size();
+        int i = 0,n = pattern.size(),m = s.size();
+        stringstream stringcap(s);
+        string word;
+        while(stringcap>>word) v.push_back(word); 
+        int o = v.size();
         if(o!=n) return false; 
         for(i=0;i<n;i++)
         {
