1698b3f1d06aacfe3fb0e5261dd5a69407cc51c5
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-01-28 00:16:47 +0200

    Time: 70 ms (99.08%), Space: 75.3 MB (6.00%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         unordered_set<int>s(nums.begin(),nums.end());
         if(s.size()!=nums.size()) return true;
         return false;
