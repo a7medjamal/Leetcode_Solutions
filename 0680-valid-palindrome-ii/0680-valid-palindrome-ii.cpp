df32ac657af74542bd46e09e7d757330fb1c88a2
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-01-30 23:52:49 +0200

    Time: 57 ms (46.94%), Space: 20.9 MB (71.28%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool ispal(string &s,int l,int r)
+    {
+        int n = s.size();
+        while(l<r)
+        {
+            if(s[l]!=s[r])
+            {
+                return false;
+            }
+            l++,r--;
+        }
+        return true;
+    }
+    
+    
+    bool validPalindrome(string s) {
+        int n = s.size();
+        int l = 0,r = n-1;
+        while(l<r)
+        {
+            if(s[l]!=s[r])
+            {
+                return ispal(s,l+1,r)||ispal(s,l,r-1);
+            }
+            l++,r--;
+        }
+        return true;
+    }
+};
