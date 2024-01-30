c9b374b3feae59b6b6cafc69722f1635aaedbbf4
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-01-31 01:35:24 +0200

    Time: 143 ms (22.21%), Space: 103.9 MB (5.21%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
         unordered_map<int,int>mp;
         for(auto i:nums) mp[i]++;
         for(auto i:mp) {if(i.second>1) return i.first;}
