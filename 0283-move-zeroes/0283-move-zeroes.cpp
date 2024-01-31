f85e8918bb8255a04f035e91fc117c9cd85f2969
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-01-31 02:09:42 +0200

    Time: 8 ms (98.08%), Space: 21.7 MB (7.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    }
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int l = 0,r = 1;
