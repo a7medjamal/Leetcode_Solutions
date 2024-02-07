e289b35146843b617936e4a7e77df14ebe671788
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-02-07 06:53:10 +0200

    Time: 37 ms (87.69%), Space: 26.9 MB (9.82%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -3,9 +3,9 @@ public:
     Solution() {
         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     }
+    unordered_map<string,int>mp;
+    vector<string>ans;
     vector<string> findRepeatedDnaSequences(string s) {
-        unordered_map<string,int>mp;
-        vector<string>ans;
         for(int i = 0;i < (int)s.size();i++)
         {
             ++mp[s.substr(i,10)];
