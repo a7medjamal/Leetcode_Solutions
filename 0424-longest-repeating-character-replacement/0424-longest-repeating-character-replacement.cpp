da456055948030b6a62630b33d2c2461bb217981
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-02-12 23:43:35 +0200

    Time: 0 ms (100.00%), Space: 8.2 MB (39.30%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -5,19 +5,19 @@ public:
         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     }
     int characterReplacement(string s, int k) {
-        unordered_map<char,int>mp;
+        int freq[26]; memset(freq,0,sizeof(freq));
         int l=0,r=0;
         int ans = 0,mx = 0;
         for(r=0;r<(int)s.size();r++)
         {
-            mp[s[r]]++;
-            mx = max(mx,mp[s[r]]);
+            freq[s[r]-'A']++;
+            mx = max(mx,freq[s[r]-'A']);
             if((r-l+1)-mx>k)
             {
-                mp[s[l]]--;
+                freq[s[l]-'A']--;
                 l++;
             }
-            else ans = max(ans,r-l+1);
+            ans = max(ans,r-l+1);
         }
         return ans;
     }
