581d87f1112945b0e9992ca822e3e1022c242df1
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-02-17 20:39:50 +0200

    Time: 682 ms (5.00%), Space: 21.2 MB (10.10%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+public:
+    MinStack() {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    }
+    deque<int>dq;
+    void push(int val) {
+        dq.push_front(val);
+    }
+    
+    void pop() {
+        dq.pop_front();
+    }
+    
+    int top() {
+        return dq.front();
+    }
+    
+    int getMin() {
+        return *min_element(dq.begin(),dq.end());
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
