76b55ca49a11f0cfa3db4cb19ece2a9c404c0f72
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-03-10 00:08:48 +0200

    Time: 4 ms (93.24%), Space: 10.5 MB (58.92%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,20 +12,17 @@ public:
         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     }
     bool hasCycle(ListNode *head) {
-        if(head==nullptr||head->next==nullptr) return false;
-        
-        ListNode* first = head;
-        ListNode* last = head;
-        
-        while(first->next) {
-            first = first->next;
-            if(first->next) {
-                first = first->next;
-                last = last->next;
+        ListNode *tmp1 = head;
+        ListNode *tmp2 = head;
+        while (tmp1 != nullptr && tmp2 != nullptr && tmp2->next != nullptr)
+        {
+            tmp1 = tmp1->next;
+            tmp2 = tmp2->next->next;
+            if (tmp1 == tmp2)
+            {
+                return true;
             }
-            if(first==last) return true;
         }
-        
         return false;
     }
 };
