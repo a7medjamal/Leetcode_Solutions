e7c21967a98579852add7b349e255d36244a8948
Ahmed Jamal Sultan <109503253+a7medjamal@users.noreply.github.com>
2024-04-01 18:07:11 +0200

    Time: 2 ms (80.00%), Space: 7.2 MB (20.00%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.cpp
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    }
     int sumOfTheDigitsOfHarshadNumber(int x) {
         int sum = 0,nm=x;
         while(x) {
