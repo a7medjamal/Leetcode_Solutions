9daeb97d396eb5d181e30d0e64d9b62fd0e2c43b
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-01-14 15:41:21 +0200

    Time: 3 ms (75.81%), Space: 88.1 MB (47.73%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -7,12 +7,9 @@ public:
         int common = 0;
         for(int i=0;i < n;++i) {
             freq[A[i]]++;
+            if(freq[A[i]]==2) ++common;
             freq[B[i]]++;
-            if(A[i]==B[i]&&freq[A[i]]>=2) ++common;
-            else {
-                if(freq[A[i]]>=2) ++common;
-                if(freq[B[i]]>=2) ++common;
-            }
+            if(freq[B[i]]==2) ++common;
             C[i]=common;
         }
         return C;
