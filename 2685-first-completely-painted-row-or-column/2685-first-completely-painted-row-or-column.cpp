145eaa25b08e3a7ede67898ecb88d7d443a319e2
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-01-20 23:44:10 +0200

    Time: 12 ms (84.04%), Space: 133.9 MB (91.87%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int n = mat.size(),m = mat[0].size();
-        unordered_map<int,pair<int,int>> index;
+        vector<pair<int,int>> index(n*m);
         for(int i = 0;i < n;++i) {
             for(int j = 0;j < m;++j) {
-                index[mat[i][j]] = make_pair(i,j);
+                index[mat[i][j]-1] = make_pair(i,j);
             }
         }
-        int ans = -1;
         vector<int> pre_row(n,0),pre_col(m,0);
         for(int i = 0;i < (int)arr.size();++i) {
-            auto [r,c] = index[arr[i]];
-            ++pre_row[r]; ++pre_col[c];
-            if(pre_row[r]==m||pre_col[c]==n) {
-                ans=i;
-                break;
+            auto [r,c] = index[arr[i]-1];
+            if(++pre_row[r]==m||++pre_col[c]==n) {
+                return i;
             }
         }
-        return ans;
+        return arr.size();
     }
 };
