998160788894f75c8e14fa66bc64f3a4d433c23e
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-01-24 21:08:56 +0200

    Time: 28 ms (64.82%), Space: 58.5 MB (62.77%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,21 +1,25 @@
 class Solution {
-    bool dfs(int u,vector<vector<int>>& graph, unordered_map<int,bool>& safe) {
-        if(safe.find(u)!=safe.end()) return safe[u];
-        safe[u]=false;
-        for(auto v:graph[u]) {
-            if(!dfs(v,graph,safe)) return false;
+    bool dfs(int u, vector<vector<int>>& graph,
+             unordered_map<int, bool>& safe) {
+        if (safe.find(u) != safe.end())
+            return safe[u];
+        safe[u] = false;
+        for (auto v : graph[u]) {
+            if (!dfs(v, graph, safe))
+                return false;
         }
-        safe[u]=true;
+        safe[u] = true;
         return true;
     }
+
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int>ans;
+        vector<int> ans;
         int n = graph.size();
         unordered_map<int, bool> safe;
-        for(int i=0;i<n;++i) {
-            if(dfs(i,graph,safe))
-              ans.push_back(i);
+        for (int i = 0; i < n; ++i) {
+            if (dfs(i, graph, safe))
+                ans.push_back(i);
         }
         return ans;
     }
