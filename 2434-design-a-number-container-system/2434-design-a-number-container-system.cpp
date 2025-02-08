49962369ede73e24b7c56e0d581b14025413a276
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-02-08 05:03:40 +0200

    Time: 100 ms (82.81%), Space: 185.6 MB (95.31%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -2,7 +2,9 @@ class NumberContainers {
 public:
     unordered_map<int,int> mp;
     unordered_map<int, priority_queue<int,vector<int>,greater<int>>> mp_num;
-    NumberContainers() {}
+    NumberContainers() {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    }
     void change(int index, int number) {
         mp[index]=number;
         mp_num[number].push(index);
