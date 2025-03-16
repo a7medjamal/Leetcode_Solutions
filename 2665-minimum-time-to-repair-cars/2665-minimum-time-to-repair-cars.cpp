e0a773ade05e0bbc7b112654e2e18e0efe1a3fa2
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-03-16 15:12:12 +0200

    Time: 59 ms (38.42%), Space: 59.4 MB (45.76%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, long long cars) {
+        long long l = 1, r =  *min_element(ranks.begin(),ranks.end()), mid, ans = LLONG_MAX, cnt;
+        r=r*cars*cars;
+        while(l<=r) {
+            mid=(l+r)>>1LL;
+            cnt = 0;
+            for(auto r:ranks) {
+                cnt += sqrtl(mid/r);
+                if(cnt>=cars) break;
+            }
+            if(cnt<cars) {
+                l=mid+1;
+            }
+            else r=mid-1,ans=mid;
+        }
+        return ans;
+    }
+};
