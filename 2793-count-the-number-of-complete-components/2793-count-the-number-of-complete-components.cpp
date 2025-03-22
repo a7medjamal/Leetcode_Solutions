c8e5cae05efb616118d7919a75c0e230d8c8206a
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-03-22 14:16:01 +0200

    Time: 42 ms (71.05%), Space: 130.1 MB (65.29%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkComplete(vector<int>& component,vector<vector<int>>& adj) {
+        int size = component.size();
+        for(auto u:component) {
+            if(adj[u].size()!=size-1) return false;
+        }
+        return true;
+    }
+    void dfs(int u,vector<bool>& vis,vector<vector<int>>& adj, vector<int>& component) {
+        vis[u]=true;
+        component.push_back(u);
+        for(int v:adj[u]) {
+            if(!vis[v]) {
+                dfs(v,vis,adj,component);
+            }
+        }
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        int ans = 0;
+        vector<vector<int>> adj(n);
+        for (auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        vector<bool> vis(n,false);
+        for(int i=0;i<n;++i) {
+            if(!vis[i]) {
+                vector<int> component;
+                dfs(i,vis,adj,component);
+                if(checkComplete(component,adj)) ++ans;
+            }
+        }
+        return ans;
+    }
+};
