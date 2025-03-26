5e871f60729a479e335778ca89fc7c78ceaaec9e
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-03-27 01:36:40 +0200

    Time: 57 ms (18.68%), Space: 90.2 MB (35.54%) - LeetHub

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> v; int n = grid.size(), m = grid[0].size();
+        for(int i=0;i<n;++i) {
+            for(int j=0;j<m;++j) {
+                v.push_back(grid[i][j]);
+            }
+        }
+        sort(v.begin(),v.end());
+        int median = v[v.size()/2];
+        int ans = 0;
+        for(int i=0;i<(int)v.size();++i) {
+            if(v[i]%x!=median%x) return -1;
+            ans += abs(v[i]-median)/x;
+        }
+        return ans;
+    }
+};
