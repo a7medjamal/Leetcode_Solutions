dbb47aaa54f39961848c42571deaca22c4e123b2
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-03-27 01:40:58 +0200

    Time: 55 ms (22.29%), Space: 89.8 MB (63.86%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
         ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-        vector<int> v; int n = grid.size(), m = grid[0].size();
-        for(int i=0;i<n;++i) {
-            for(int j=0;j<m;++j) {
+        vector<int> v;
+        for(int i=0;i<grid.size();++i) {
+            for(int j=0;j<grid[0].size();++j) {
                 v.push_back(grid[i][j]);
             }
         }
         sort(v.begin(),v.end());
         int median = v[v.size()/2];
         int ans = 0;
-        for(int i=0;i<(int)v.size();++i) {
+        for(int i=0;i<v.size();++i) {
             if(v[i]%x!=median%x) return -1;
             ans += abs(v[i]-median)/x;
         }
