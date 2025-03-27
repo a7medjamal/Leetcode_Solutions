12fe4889a46ee48c5d0b6591ec58bed0ab321ce8
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-03-27 15:49:59 +0200

    Time: 0 ms (100%), Space: 89.9 MB (85.66%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        unordered_map<int,int> freq;
-        int n = nums.size();
-        int ans = 0;
-        for(auto& i:nums) ++freq[i];
-        for(auto& i:nums) {
-            if(freq[i]>(n/2)) {
-                ans=i;
-                break;
-            }
+        int n=nums.size(), d = nums[0],freq = 1;
+        for(int i=1;i<n;++i){
+            if(nums[i]!=d) --freq;
+            else ++freq;
+            if(!freq) d = nums[i],freq = 1;
         }
-        if(!ans) return -1;
-        int l=0,r=freq[ans];
-        for(int i=0;i<n-1;++i) {
-            if(nums[i]==ans) {
-                ++l,--r;
-            }
-            if(l>((i+1)/2)&&r>(n-i-1)/2) return i;
+        int max_freq = 0;
+        for(int i=0;i<n;++i) if(nums[i]==d) max_freq++;
+        int prefix_count = 0;
+        for(int i=0;i<n-1;++i){
+            if(nums[i]==d) prefix_count++,max_freq--;
+            if((prefix_count > (i+1)/2) and (max_freq > (n-i-1)/2)) return i;
         }
         return -1;
     }
-};
+}; 
