f3f12564af2abc8788474eb845faa182e95971f4
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-03-27 15:46:48 +0200

    Time: 27 ms (62.3%), Space: 98.6 MB (56.56%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        unordered_map<int,int> mp1,mp2,freq;
+        unordered_map<int,int> freq;
         int n = nums.size();
         int ans = 0;
         for(auto& i:nums) ++freq[i];
@@ -13,7 +13,7 @@ public:
         }
         if(!ans) return -1;
         int l=0,r=freq[ans];
-        for(int i=0;i<n;++i) {
+        for(int i=0;i<n-1;++i) {
             if(nums[i]==ans) {
                 ++l,--r;
             }
