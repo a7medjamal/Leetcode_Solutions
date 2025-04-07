0422c13a446723aa1b4a9531579ebae50ae4a4a0
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-04-07 21:20:02 +0200

    Time: 0 ms (100%), Space: 14.2 MB (84.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -4,7 +4,7 @@ public:
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum&1) return false;
         const int N = 1e4+5;
-        bitset<N> dp; dp[0] = 1;
+        bitset<N> dp(1);
         for(auto i:nums) {
             dp |= dp << i;
             if(dp[sum/2]) return true;
