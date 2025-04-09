989af56446c1f590d55202de9d1a933767779551
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-04-09 10:23:06 +0200

    Time: 7 ms (89.94%), Space: 32.5 MB (42.14%) - LeetHub

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        for(auto i:nums) {if(i>k) ++freq[i]; if(i<k) return -1;}
+        return freq.size();
+    }
+};
