bde6848f6420c5312d829f6dca06cd95baa84467
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2025-06-19 07:42:31 +0300

    Time: 36 ms (48.25%), Space: 86.9 MB (39.38%) - LeetHub

---
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int ans = 0, mn = nums[0];
+        for(int i=1;i<nums.size();++i) {
+            if((nums[i]-mn)>k) ++ans,mn=nums[i];
+        }
+        return ans+1;
+    }
+};
