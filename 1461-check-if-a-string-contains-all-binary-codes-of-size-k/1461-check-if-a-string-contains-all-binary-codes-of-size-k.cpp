0f9873b71f71c9eb8c6d6e9b9362b2d94823997e
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2026-02-23 17:28:38 +0200

    Time: 557 ms (26.08%), Space: 150.5 MB (26.81%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -3,14 +3,12 @@ public:
     bool hasAllCodes(string s, int k) {
         unordered_set<string> allsubs;
         string sub="";
-        map<string,int> mp;
         for(int i=0;i<(int)s.size();++i) {
             if(i+k<=(int)s.size()) {
                 sub=s.substr(i,k);
                 allsubs.insert(sub);
-                ++mp[sub];
             }
         }
-        return((mp.size()==(1<<k))?true:false);
+        return allsubs.size()==(1<<k);
     }
 };
