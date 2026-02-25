fa85a55f94f626c08a269e18851b6c3e3964fac5
Ahmed Jamal Sultan <ahmedjmal20003@gmail.com>
2026-02-25 14:40:11 +0200

    Time: 0 ms (100%), Space: 14.1 MB (79.86%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    static bool cmp(int a,int b) {
-        if(__builtin_popcount(a)==__builtin_popcount(b)) return a<b;
-        return __builtin_popcount(a)<__builtin_popcount(b);
-    }
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(),arr.end(),cmp);
+        sort(arr.begin(), arr.end(), [](int a, int b) {
+            if (__builtin_popcount(a) == __builtin_popcount(b))
+                return a < b;
+            return __builtin_popcount(a) < __builtin_popcount(b);
+        });
         return arr;
     }
 };
